9283c51420a4f0591bc12b06606d83b607b12723
Ivan <ivndev0@gmail.com>
2023-01-14 18:18:19 -0300

    refactor: DB connection is now performed before listening to the HTTP port

--- src/app.ts
+++ src/app.ts
@@ -1,23 +1,27 @@
 import 'reflect-metadata';
+import DBConnection from './config/dbConnect';
 import { InversifyExpressServer } from 'inversify-express-utils';
 import { Container } from 'inversify';
 import TYPES from './ioc/ioc-types';
 import { CpfServices } from './services/cpfServices';
 import './controllers/cpfsController';
-import DBConnection from './config/dbConnect';
 
 const container = new Container({
 	autoBindInjectable: true,
 	defaultScope: 'Singleton',
+	skipBaseClassChecks: true,
 });
 
 container.bind<CpfServices>(TYPES.CpfServices).to(CpfServices);
 
 const server = new InversifyExpressServer(container);
 
-const serverInstance = server.build();
-serverInstance.listen(3030, () => {
-	console.log('Server listening on port 3030');
-});
 const db = new DBConnection();
 void db.connect();
+const serverInstance = server.build();
+
+setTimeout(() => {
+	serverInstance.listen(3030, () => {
+		console.log('Server listening on port 3030');
+	});
+}, 3000);
